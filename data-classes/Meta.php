14ecfe49fd9f567c6c531eedbe926f9e14d09a74
Jason Suarez <swarecito@gmail.com>
2024-07-13 22:23:09 +0200

    feat: Add Meta and Season data classes

---
+++ data-classes/Meta.php
@@ -0,0 +1,8 @@
+<?php
+
+class Meta
+{
+    public function __construct()
+    {
+    }
+}
---
+++ data-classes/Seasons.php
@@ -0,0 +1,24 @@
+<?php
+require 'Data.php';
+require 'Meta.php';
+
+class Season
+{
+    /** @var Data[] */
+    public array $data;
+    public Links $links;
+    public Meta $meta;
+
+    /**
+     * @param Data[] $data
+     */
+    public function __construct(
+        array $data,
+        Links $links,
+        Meta $meta
+    ) {
+        $this->data = $data;
+        $this->links = $links;
+        $this->meta = $meta;
+    }
+}
