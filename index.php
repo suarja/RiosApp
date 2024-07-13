f2fd455ff88139d711d9d145347ab0b3e114c5bb
Jason Suarez <swarecito@gmail.com>
2024-07-13 23:54:34 +0200

    Refactor player-1.php to use new Attributes class

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-include 'functions.php';
+require "data/player-1.php";
+require './src/functions.php';
 $heading = "RiosApp";
 
+
+
+
+// $player = new Player(
+//     $player,
+//     new Links($player['links']['self']),
+//     new Meta($player['meta'])
+// );
