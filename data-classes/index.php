dc2e32ed4269bb6d86ba8e34a94f0882025315a1
Jason Suarez <swarecito@gmail.com>
2024-07-13 23:06:30 +0200

    feat: Add data classes for Assets, Attributes, Data, Links, Matches, Player, Relationships, Meta, Seasons, and Matches

---
+++ data-classes/index.php
@@ -0,0 +1,10 @@
+<?php
+require "Assets.php";
+require "Attributes.php";
+require "Links.php";
+require "Meta.php";
+require "Player.php";
+require "Relationships.php";
+require "Seasons.php";
+require "Matches.php";
+require "Data.php";
