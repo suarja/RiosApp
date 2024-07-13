fd6d228a3b48ea7e4dcad735a73ed6ed9558f66a
Jason Suarez <swarecito@gmail.com>
2024-07-13 23:11:58 +0200

    Refactor Player class to use new Attributes class

--- data-classes/Player.php
+++ data-classes/Player.php
@@ -21,4 +21,35 @@ class Player
         $this->relationships = $relationships;
         $this->links = $links;
     }
+
+    public static function fromJSON(
+        string $jsonString
+    ): Player {
+        $player = json_decode($jsonString, true);
+        return new Player(
+            $player['type'],
+            $player['id'],
+            new Attributes(
+                $player['attributes']['stats'] ?? "",
+                $player['attributes']['titleId'],
+                $player['attributes']['shardId'],
+                $player['attributes']['clanId'],
+                $player['attributes']['name'],
+                $player['attributes']['banType'],
+                $player['attributes']['patchVersion'],
+            ),
+            new Relationships(
+                new Assets(
+                    $player['relationships']['assets']['data'],
+                ),
+                new Matches(
+                    $player['relationships']['matches']['data'],
+                ),
+            ),
+            new Links(
+                $player['links']['self'],
+                $player['links']['schema'],
+            ),
+        );
+    }
 }
