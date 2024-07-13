b04e3dc2faf774627a22c801c7be401c97adbde3
Jason Suarez <swarecito@gmail.com>
2024-07-14 01:57:28 +0200

    Refactor Seasons class to include static factory method for object creation

--- data-classes/Seasons.php
+++ data-classes/Seasons.php
@@ -8,7 +8,7 @@ class Season
     public Meta $meta;
 
     /**
-     * @param Data[] $data
+     * @param SeasonData[] $data
      */
     public function __construct(
         array $data,
@@ -18,5 +18,14 @@ class Season
         $this->data = $data;
         $this->links = $links;
         $this->meta = $meta;
+    }
+
+    public static function fromJSON(string $jsonString): Season
+    {
+        $data = json_decode($jsonString, true)["data"];
+        $data = array_map(fn($data) => Data::fromArray($data), $data);
+        $links = Links::fromArray(json_decode($jsonString, true)["links"]);
+        $meta = Meta::fromArray(json_decode($jsonString, true)["meta"]);
+        return new Season($data, $links, $meta);
     }
 }
