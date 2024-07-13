48c2ae43ca9b4a515e63090a7e841ce907ce45f9
Jason Suarez <swarecito@gmail.com>
2024-07-13 23:53:53 +0200

    Refactor player-1.php to use new Attributes class

--- data/player-1.php
+++ data/player-1.php
@@ -1,5 +1,5 @@
 <?php
-
-$playerOnePath = "data/player-1.json";
+include "data-classes/index.php";
+$playerOnePath = "./data/player-1.json";
 $playerOneJsonString = file_get_contents($playerOnePath);
 $playerOne = Player::fromJSON($playerOneJsonString);
