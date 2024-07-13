a3ffafbf45ddcb1e6dc000f4d020c3ca5933186f
Jason Suarez <swarecito@gmail.com>
2024-07-13 23:12:02 +0200

    Refactor player-1.php to use new Attributes class

--- data/player-1.php
+++ data/player-1.php
@@ -2,32 +2,4 @@
 
 $playerOnePath = "data/player-1.json";
 $playerOneJsonString = file_get_contents($playerOnePath);
-$playerOne = json_decode($playerOneJsonString, true);
-$playerOne = new Player(
-    $playerOne['type'],
-    $playerOne['id'],
-    new Attributes(
-        $playerOne['attributes']['stats'] ?? "",
-        $playerOne['attributes']['titleId'],
-        $playerOne['attributes']['shardId'],
-        $playerOne['attributes']['clanId'],
-        $playerOne['attributes']['name'],
-        $playerOne['attributes']['banType'],
-        $playerOne['attributes']['patchVersion'],
-
-
-    ),
-    new Relationships(
-        new Assets(
-            $playerOne['relationships']['assets']['data'],
-
-        ),
-        new Matches(
-            $playerOne['relationships']['matches']['data'],
-        ),
-    ),
-    new Links(
-        $playerOne['links']['self'],
-        $playerOne['links']['schema'],
-    ),
-);
+$playerOne = Player::fromJSON($playerOneJsonString);
