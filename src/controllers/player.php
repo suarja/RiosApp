b0760e94665e61125a1c797196ce58c484d8dc91
Jason Suarez <swarecito@gmail.com>
2024-07-15 01:52:06 +0200

    feat: Add player form submission handling in player controller

--- src/controllers/player.php
+++ src/controllers/player.php
@@ -7,15 +7,34 @@ require "./data/player-1.php";
 require "./src/functions.php";
 require "./data/seasons-list.php";
 require "./config.php";
+require_once './src/Database.php';
 
 $heading = "Player Stats";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $playerName = $_POST['playerName'];
-    $playerOne = getPlayer($playerName, $PUBG_API_KEY);
-    $playerSeasonStats = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
-
-    require "./views/player.view.php";
+    // Check if the player name is empty
+    if (empty($playerName)) {
+        $error = "Please enter a player name";
+        require "./views/partials/player-form.php";
+        exit;
+    }
+    // check in the database if the player exists
+    $isPlayerInDatabase = $db->isPlayerInDatabaseFromName($playerName);
+    if ($isPlayerInDatabase) {
+        $playerOne = $db->getPlayerFromName($playerName);
+        $playerOne = Player::fromDB($playerOne);
+        $playerSeasonStats
+            = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
+        require "./views/player.view.php";
+        exit;
+    } else {
+        $playerOne = getPlayer($playerName, $PUBG_API_KEY);
+        $db->insertPlayer($playerOne);
+        $playerSeasonStats = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
+        require "./views/player.view.php";
+        exit;
+    }
 } else {
     require "./views/partials/player-form.php";
 }
