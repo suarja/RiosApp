92df31475098ad033f98cdb719cfcf05467f2506
Jason Suarez <swarecito@gmail.com>
2024-07-15 01:00:51 +0200

    chore: Update player controller to handle form submission

--- src/controllers/player.php
+++ src/controllers/player.php
@@ -7,7 +7,15 @@ require "./data/player-1.php";
 require "./src/functions.php";
 require "./data/seasons-list.php";
 require "./config.php";
-$playerSeasonStats = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
 
-require "views/player.view.php";
-?>
+$heading = "Player Stats";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $playerName = $_POST['playerName'];
+    $playerOne = getPlayer($playerName, $PUBG_API_KEY);
+    $playerSeasonStats = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
+
+    require "./views/player.view.php";
+} else {
+    require "./views/partials/player-form.php";
+}
