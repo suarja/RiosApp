58cff165bcbceea2e93404f8c0b8102bf4bb509e
Jason Suarez <swarecito@gmail.com>
2024-07-14 23:39:41 +0200

    Refactor index.php to use require_once for including class files

--- data-classes/index.php
+++ data-classes/index.php
@@ -1,14 +1,14 @@
 <?php
-require "Assets.php";
-require "Attributes.php";
-require "Links.php";
-require "Meta.php";
-require "Player.php";
-require "Relationships.php";
-require "Seasons.php";
-require "Matches.php";
-require "Data.php";
-require "SeasonData.php";
-require "SeasonAttributes.php";
-require "SeasonLink.php";
-require "PlayerSeasonStats.php";
+require_once "Assets.php";
+require_once "Attributes.php";
+require_once "Links.php";
+require_once "Meta.php";
+require_once "Player.php";
+require_once "Relationships.php";
+require_once "Seasons.php";
+require_once "Matches.php";
+require_once "Data.php";
+require_once "SeasonData.php";
+require_once "SeasonAttributes.php";
+require_once "SeasonLink.php";
+require_once "PlayerSeasonStats.php";
