a8ab7f3c94814888fc3067931be1cdad8a25d428
Jason Suarez <swarecito@gmail.com>
2024-07-15 01:51:31 +0200

    feat: Add static method to create Player from database array

--- data-classes/Player.php
+++ data-classes/Player.php
@@ -52,4 +52,37 @@ class Player
             ),
         );
     }
+
+
+    // Static method to create a Player from a database array
+    public static function fromDB(array $dbArray): Player
+    {
+        $attributes = new Attributes(
+            $dbArray['stats'] ?? "",
+            $dbArray['title_id'],
+            $dbArray['shard_id'],
+            $dbArray['clan_id'],
+            $dbArray['name'],
+            $dbArray['ban_type'],
+            $dbArray['patch_version']
+        );
+
+        $assets = new Assets(json_decode($dbArray['assets_json'], true));
+        $matches = new Matches(json_decode($dbArray['matches_json'], true));
+
+        $relationships = new Relationships($assets, $matches);
+
+        $links = new Links(
+            $dbArray['link_self'],
+            $dbArray['link_schema']  // Assuming this might be empty but structured for future use
+        );
+
+        return new Player(
+            $dbArray['type'],
+            $dbArray['id'],
+            $attributes,
+            $relationships,
+            $links
+        );
+    }
 }
