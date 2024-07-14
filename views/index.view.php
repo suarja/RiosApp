eebb19188d50e469f496fbbfb5efd23e25033d80
Jason Suarez <swarecito@gmail.com>
2024-07-14 23:38:49 +0200

    Refactor into own file

--- views/index.view.php
+++ views/index.view.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" class="dark-mode">
+<?php require './views/partials/head.php'; ?>
 
-<head>
-  <style>
-    html {
-      background-color: black;
-    }
 
-    body {
-      background-color: white;
-      color: black;
-      font-size: 25px;
-    }
-
-    .dark-mode {
-      background-color: black;
-      color: white;
-    }
-
-    .light-mode {
-      background-color: white;
-      color: black;
-    }
-  </style>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title><?= $heading; ?></title>
-</head>
-
-<body class="dark-mode">
-
-  <?php require './partials/landing.view.php'; ?>
-  <?php require './partials/player.view.php'; ?>
-  <?php require './partials/seasons-list.php'; ?>
-</body>
-
-</html>
+<?php require './views/partials/footer.php'; ?>
---
+++ views/partials/head.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" class="dark-mode">
+
+<head>
+    <style>
+        html {
+            background-color: black;
+        }
+
+        body {
+            background-color: white;
+            color: black;
+            font-size: 25px;
+        }
+
+        .dark-mode {
+            background-color: black;
+            color: white;
+        }
+
+        .light-mode {
+            background-color: white;
+            color: black;
+        }
+    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $heading; ?></title>
+</head>
