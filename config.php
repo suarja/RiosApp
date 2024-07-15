e67a1d35f181f8bd23b1f1e731475ce8623ec1c7
Jason Suarez <swarecito@gmail.com>
2024-07-15 17:35:52 +0200

    chore: Update config.php to use environment variables for database configuration

--- config.php
+++ config.php
@@ -16,5 +16,40 @@ if (!$PUBG_API_KEY) {
 }
 
 
+$DB_URL = $_SERVER['DB_URL'] ?? null;
+$DB_USER = $_SERVER['DB_USER'] ?? null;
+$DB_PASSWORD = $_SERVER['DB_PWD'] ?? null;
+$DB_HOST = $_SERVER['DB_HOST'] ?? null;
+$DB_PORT = $_SERVER['DB_PORT'] ?? null;
+$DB_NAME = $_SERVER['DB_NAME'] ?? null;
+
+if (!$DB_URL) {
+    throw new Exception('No database URL provided. Please add a DB_URL to the .env file.');
+} else if (!$DB_USER) {
+    throw new Exception('No database user provided. Please add a DB_USER to the .env file.');
+} else if (!$DB_PASSWORD) {
+    throw new Exception('No database password provided. Please add a DB_PWD to the .env file.');
+} else if (!$DB_HOST) {
+    throw new Exception('No database host provided. Please add a DB_HOST to the .env file.');
+} else if (!$DB_PORT) {
+    throw new Exception('No database port provided. Please add a DB_PORT to the .env file.');
+} else if (!$DB_NAME) {
+    throw new Exception('No database name provided. Please add a DB_NAME to the .env file.');
+}
+
+
+function dsn($DB_HOST, $DB_PORT, $DB_NAME)
+{
+    return "mysql:host={$DB_HOST};port={$DB_PORT};dbname={$DB_NAME}";
+}
+
+$dsn = dsn($DB_HOST, $DB_PORT, $DB_NAME);
+
+ $DB_CONFIG = [
+    'dsn' => $dsn,
+    'user' => $DB_USER,
+    'password' => $DB_PASSWORD
+];
+
 
 
