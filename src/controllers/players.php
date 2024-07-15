b9b875ce791fd2d053886ca761784ecb1e122f68
Jason Suarez <swarecito@gmail.com>
2024-07-15 22:33:28 +0200

    Update players controller to fetch players from the database and display their stats in the view

--- src/controllers/players.php
+++ src/controllers/players.php
@@ -2,4 +2,24 @@
 require base_path('/src/data/player-1.php');
 $heading = 'Players';
 
-require view('players', compact('heading'));
+use Core\Database;
+
+require base_path("/config.php");
+require base_path("/src/data/seasons-list.php");
+require base_path('/src/core/Database.php');
+require base_path('/data-classes/PlayerWithStats.php');
+
+$db = new Database($DB_CONFIG);
+$seasonId;
+$PUBG_API_KEY;
+// Get all players from the database
+$players = $db->getPlayers();
+$teamPlayers = [];
+foreach ($players as $player) {
+    $player = Player::fromDB($player);
+    $playerSeasonStats = getPlayerSeasonStats($player, $seasonId, $PUBG_API_KEY);
+    $teamPlayers[] = new PlayerWithStats($player, $playerSeasonStats);
+}
+
+require view('players', compact('heading', 'teamPlayers'));
+
