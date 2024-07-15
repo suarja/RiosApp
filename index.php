d62fa0a0a84bd0a04379f19a4516a73b61e90ca9
Jason Suarez <swarecito@gmail.com>
2024-07-16 01:24:14 +0200

    refactor: Update index.php to use Router class for routing

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Router\Router;
+
 const BASE_PATH = __DIR__;
 
 require BASE_PATH . '/functions.php';
@@ -8,8 +10,20 @@ $heading = "RiosApp";
 
 require view("index", ["heading" => $heading]);
 
-require base_path('/src/router/router.php');
 
 // spl_autoload_register(function ($class) {
 //     require base_path("/src/core/{$class}.php");
 // });
+
+require base_path('/src/router/Router.php');
+$router = new Router();
+
+require base_path('/src/router/routes.php');
+
+
+$method = $_POST["_method"] ?? $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+$router->route(
+    $uri,
+    $method
+);
