fe94d85f8515c0fbb82095ad661ab884119ae66e
Jason Suarez <swarecito@gmail.com>
2024-07-16 00:53:42 +0200

    refactor: Add player creation functionality to store.php

---
+++ src/controllers/store.php
@@ -0,0 +1,48 @@
+<?php
+
+use Core\Database;
+
+$heading = "Create Player";
+
+// Load the database
+require base_path("/config.php");
+require base_path('/src/core/Database.php');
+require base_path('/data-classes/index.php');
+
+$db = new Database($DB_CONFIG);
+
+$errors = [];
+$playerName = $_POST['playerName'];
+// Check if the player name is empty
+if (empty($playerName)) {
+    $errors[] = "Please enter a player name";
+    require view('players/player-form', compact('heading', 'errors'));
+    exit;
+}
+// check in the database if the player exists
+$isPlayerInDatabase = $db->isPlayerInDatabaseFromName($playerName);
+if ($isPlayerInDatabase) {
+    $errors[] = "Player already exists";
+    require view('players/player-form', compact('heading', 'errors'));
+    exit;
+} else {
+    $player = getPlayer($playerName, $PUBG_API_KEY);
+    if (!$player) {
+        $errors[] = "Player not found";
+        require view('players/player-form', compact('heading', 'errors'));
+        exit;
+    }
+    $db->insertPlayer($player);
+
+    //* Add the stats to DB
+    // $playerSeasonStats = getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY);
+    // $db->insertPlayerSeasonStats($playerOne, $playerSeasonStats);
+
+    //? Consider redirect to the player page
+    // require view('players/player', compact('heading', 'playerOne', 'playerSeasonStats'));
+
+    redirect("/players");
+    exit;
+}
+
+//hmidoulye
