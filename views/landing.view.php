d6f2662268bf3ec311d4a24351b69998d3da9fd6
Jason Suarez <swarecito@gmail.com>
2024-07-16 01:18:07 +0200

    refactor: Update landing.view.php to improve landing page layout

--- views/landing.view.php
+++ views/landing.view.php
@@ -1,11 +1,11 @@
 <?php base_path("/views/partials/head.php"); ?>
 
-<main class="h-screen">
-  <div class="bg-white h-full w-full">
+<main class="h-screen bg-white">
+  <div class="bg-white h-min-screen w-full">
     <?php require base_path("/views/partials/header.php"); ?>
 
 
-    <div class="relative isolate px-6 pt-14 lg:px-8">
+    <div class="relative isolate px-6 pt-14 lg:px-8 ">
       <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="
           clip-path: polygon(
