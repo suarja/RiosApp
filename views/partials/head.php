e2b79262f6710202ad3385e522ab958aad6801c1
Jason Suarez <swarecito@gmail.com>
2024-07-16 01:17:08 +0200

    refactor: Update viewport meta tag in head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -8,7 +8,6 @@
         }
 
         body {
-            background-color: white;
             color: black;
             font-size: 25px;
         }
@@ -25,6 +24,6 @@
     </style>
     <script src="https://cdn.tailwindcss.com"></script>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover" />
     <title><?= $heading; ?></title>
 </head>
