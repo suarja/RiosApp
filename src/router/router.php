eb49229106aea62fb5025a2575a4865464793b85
Jason Suarez <swarecito@gmail.com>
2024-07-15 23:58:11 +0200

    refactor: Update router.php to use Router class for routing

--- src/router/router.php
+++ src/router/router.php
@@ -1,25 +1,48 @@
 <?php
 
-$routes = [
-    '/' => base_path('/src/controllers/landing.php'),
-    '/players' => base_path('/src/controllers/players.php'),
-    '/player' => base_path('/src/controllers/player.php'),
-    '/players/create' => base_path('/src/controllers/create-player.php'),
-];
+namespace Router;
 
-function routeToController($routes)
+require base_path("/src/router/routes.php");
+// function routeToController($routes)
+// {
+//     $uri = $_SERVER['REQUEST_URI'];
+//     // Normalize the URI to ensure routing works as expected
+//     $uri = parse_url($uri, PHP_URL_PATH);
+
+//     if (array_key_exists($uri, $routes)) {
+//         require $routes[$uri];
+//     } else {
+//         http_response_code(404);
+//         echo "404 Not Found";
+//     }
+// }
+
+class Router
 {
-    $uri = $_SERVER['REQUEST_URI'];
-    // Normalize the URI to ensure routing works as expected
-    $uri = parse_url($uri, PHP_URL_PATH);
+    protected $routes = [];
 
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        http_response_code(404);
-        echo "404 Not Found";
+    public function get($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'GET'
+        ];
+    }
+    public function post($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'POST'
+        ];
+    }
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require $route['controller'];
+            }
+        }
     }
 }
-
-// Call the routing function
-routeToController($routes);
