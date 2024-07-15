2ef95558ebfaf58b16a21f287ed095db2c50316a
Jason Suarez <swarecito@gmail.com>
2024-07-16 00:01:18 +0200

    refactor: Update player-form.view.php to use correct partial path for player template

--- views/player-form.view.php
+++ views/player-form.view.php
@@ -1,3 +1,3 @@
 <?php require base_path("/views/partials/head.php"); ?>
-<?php require base_path("/views/partials/player.php"); ?>
+<?php require base_path("/views/partials/players/player.php"); ?>
 <?php require base_path("/views/partials/footer.php"); ?>
