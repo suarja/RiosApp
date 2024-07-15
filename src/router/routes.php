8e04dc4f85035e40f2d95d8324cfee034ebf77bf
Jason Suarez <swarecito@gmail.com>
2024-07-16 00:53:12 +0200

    refactor: Add new routes for player store and creation

--- src/router/routes.php
+++ src/router/routes.php
@@ -6,5 +6,6 @@ $router = new Router();
 $router->get('/', base_path('/src/controllers/landing.php'));
 $router->get('/players', base_path('/src/controllers/players.php'));
 $router->get('/player', base_path('/src/controllers/player.php'));
+$router->get('/player/store', base_path('/src/controllers/store-form.php'));
+$router->post('/player/store', base_path('/src/controllers/store.php'));
 $router->get('/players/create', base_path('/src/controllers/create-player.php'));
-
