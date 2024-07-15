ba643efb88302daa4b40a04b8ed18a8aff9abff8
Jason Suarez <swarecito@gmail.com>
2024-07-15 17:52:44 +0200

    chore: Update index.php to include router and remove autoloader

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+const BASE_PATH = __DIR__ . '/..';
+
+require BASE_PATH . '/functions.php';
+
+$heading = "RiosApp";
+
+require view("index", ["heading" => $heading]);
+
+require base_path('/src/router/router.php');
+
+// spl_autoload_register(function ($class) {
+//     require base_path("/src/core/{$class}.php");
+// });
