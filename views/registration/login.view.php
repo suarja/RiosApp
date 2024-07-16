08d8ba3c9c16cc48a2e5ac9f560ecf6e573c748e
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:25:31 +0200

    Update login view to include sign-in form and styling

---
+++ views/registration/login.view.php
@@ -0,0 +1,54 @@
+<?php base_path("/views/partials/head.php"); ?>
+
+<main class="h-screen bg-gray-200">
+    <div class=" h-full w-full">
+        <?php require base_path("/views/partials/header.php"); ?>
+
+
+        <div class="flex h-full flex-col justify-center items-center px-6 pt-14 py-12 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" method="POST">
+                    <input type="hidden" name="">
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div class="mt-2">
+                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                        <?php if (isset($errors['email'])) : ?>
+                            <p class="text-red-500 text-sm"><?= $errors['email'] ?></p>
+                        <?php endif; ?>
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+
+                        </div>
+                        <div class="mt-2">
+                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                        <?php if (isset($errors['password'])) : ?>
+                            <p class="text-red-500 text-sm"><?= $errors['password'] ?></p>
+                        <?php endif; ?>
+                    </div>
+
+                    <div>
+                        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                    </div>
+                    <!-- Create account option -->
+                    <div class="text-center">
+                        <a href="/register" class="text-sm font-semibold leading-6 text-gray-900">Create an account <span aria-hidden="true">→</span></a>
+                    </div>
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+</main>
+<?php base_path("/views/partials/foooter.php"); ?>
