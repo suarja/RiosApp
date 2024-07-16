6def4ff1a8bce4552aba3629a7008ca4ac1f4862
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:17:04 +0200

    Refactor Container class to throw exception when key is not found in container

--- src/core/Container.php
+++ src/core/Container.php
@@ -11,8 +11,12 @@ class Container
 
     public function resolve($key)
     {
+        if (!array_key_exists($key, $this->bindings)) {
+            throw new Exception("Key {$key} not found in container");
+        }
+
         if (isset($this->bindings[$key])) {
-            return $this->bindings[$key];
+            return call_user_func($this->bindings[$key]);
         }
     }
 }
