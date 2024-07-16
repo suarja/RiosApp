f68e1ff38a6499d067f669a39593d2c0e3cb811f
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:25:03 +0200

    Refactor registration view to update sign-in text and fix formatting

--- views/registration/register.view.php
+++ views/registration/register.view.php
@@ -8,7 +8,7 @@
         <div class="flex h-full flex-col justify-center items-center px-6 pt-14 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
 
-                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create an account</h2>
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -40,7 +40,7 @@
                     <div>
                         <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                     </div>
-                   
+
                 </form>
 
 
