7c2e060fcb4c4f22f5ff395776cdb78d3b757585
Jason Suarez <swarecito@gmail.com>
2024-07-16 02:12:21 +0200

    refactor: Remove commented out code for routing in router.php

--- src/router/router.php
+++ src/router/router.php
@@ -3,19 +3,6 @@
 namespace Router;
 
 require base_path("/src/router/routes.php");
-// function routeToController($routes)
-// {
-//     $uri = $_SERVER['REQUEST_URI'];
-//     // Normalize the URI to ensure routing works as expected
-//     $uri = parse_url($uri, PHP_URL_PATH);
-
-//     if (array_key_exists($uri, $routes)) {
-//         require $routes[$uri];
-//     } else {
-//         http_response_code(404);
-//         echo "404 Not Found";
-//     }
-// }
 
 class Router
 {
