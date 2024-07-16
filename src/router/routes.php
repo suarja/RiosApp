d38ce38b4f1bb127d86b60bed6862c41c5145e5e
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:41:37 +0200

    feat: Add login functionality to registration process

--- src/router/routes.php
+++ src/router/routes.php
@@ -10,3 +10,4 @@ $router->post('/player/store', base_path('/src/controllers/players/store.php'));
 $router->get('/register', base_path('/src/controllers/registration/index.php'));
 $router->post('/register', base_path('/src/controllers/registration/store.php'));
 $router->get('/login', base_path('/src/controllers/registration/login.php'));
+$router->post('/login', base_path('/src/controllers/registration/auth.php'));
