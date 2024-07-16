d80d5a7f9b6118acdb08ad27510b9a55603ba584
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:24:54 +0200

    Update login link in header to point to "/login" instead of "/register"

--- views/partials/header.php
+++ views/partials/header.php
@@ -22,7 +22,7 @@
           <a href="/players" class="text-sm font-semibold leading-6 text-gray-900">La Team</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="/register" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+          <a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
       </nav>
       <!-- Mobile menu, show/hide based on menu open state. -->
