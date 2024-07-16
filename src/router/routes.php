8de59f94dd827a0e93975d2f9108b4080fdeae90
Jason Suarez <swarecito@gmail.com>
2024-07-16 22:29:35 +0200

    chore: Add registration route and controller

--- src/router/routes.php
+++ src/router/routes.php
@@ -5,4 +5,7 @@ $router->get('/players', base_path('/src/controllers/players/index.php'));
 $router->get('/player', base_path('/src/controllers/players/show.php'));
 $router->get('/player/store', base_path('/src/controllers/players/create.php'));
 $router->post('/player/store', base_path('/src/controllers/players/store.php'));
+
+
 $router->get('/register', base_path('/src/controllers/registration/index.php'));
+$router->post('/register', base_path('/src/controllers/registration/store.php'));
