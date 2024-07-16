b34e45837d1164b398550ee5e60420d55e40b6b6
Jason Suarez <swarecito@gmail.com>
2024-07-16 11:40:21 +0200

    Debugging router.  Going back to old router

--- src/router/router.php
+++ src/router/router.php
@@ -1,53 +1,57 @@
 <?php
 
-
-// function routeToController($routes)
-// {
-//     $uri = $_SERVER['REQUEST_URI'];
-//     // Normalize the URI to ensure routing works as expected
-//     $uri = parse_url($uri, PHP_URL_PATH);
-
-//     if (array_key_exists($uri, $routes)) {
-//         require $routes[$uri];
-//     } else {
-//         http_response_code(404);
-//         echo "404 Not Found";
-//     }
-// }
-
-class Router
+require base_path('/src/router/routes.php');
+function routeToController($routes)
 {
-    protected $routes = [];
+    $uri = $_SERVER['REQUEST_URI'];
+    // Normalize the URI to ensure routing works as expected
+    $uri = parse_url($uri, PHP_URL_PATH);
 
-    public function get($uri, $controller)
-    {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'GET'
-        ];
-    }
-    public function post($uri, $controller)
-    {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'POST'
-        ];
-    }
-    public function route($uri, $method)
-    {
-        foreach ($this->routes as $route) {
-            if ($route['uri'] === $uri && $route['method'] === $method) {
-                require $route['controller'];
-            }
-        }
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        http_response_code(404);
+        echo "404 Not Found";
     }
 }
 
-$router = new Router();
-require base_path('/src/router/routes.php');
-$router->route(
-    $uri,
-    $method
+// class Router
+// {
+//     protected $routes = [];
+
+//     public function get($uri, $controller)
+//     {
+//         $this->routes[] = [
+//             'uri' => $uri,
+//             'controller' => $controller,
+//             'method' => 'GET'
+//         ];
+//     }
+//     public function post($uri, $controller)
+//     {
+//         $this->routes[] = [
+//             'uri' => $uri,
+//             'controller' => $controller,
+//             'method' => 'POST'
+//         ];
+//     }
+//     public function route($uri, $method)
+//     {
+//         foreach ($this->routes as $route) {
+//             if ($route['uri'] === $uri && $route['method'] === $method) {
+//                 require $route['controller'];
+//             }
+//         }
+//     }
+// }
+
+// $router = new Router();
+// require base_path('/src/router/routes.php');
+// $router->route(
+//     $uri,
+//     $method
+// );
+
+routeToController(
+    $routes
 );
