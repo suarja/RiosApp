cbbe5c8aa0cabdf30eba9cd40e59965ee05ddbd0
Jason Suarez <swarecito@gmail.com>
2024-07-16 23:17:10 +0200

    Refactor App.php to use dependency injection container pattern

---
+++ src/core/App.php
@@ -0,0 +1,27 @@
+<?php
+
+require base_path("/src/core/Container.php");
+require base_path("/src/core/Database.php");
+class App
+{
+    public static function setContainer()
+    {
+        $container = new Container();
+        $container->bind('db', function () {
+            require base_path("/config.php");
+            return new Database(
+                $DB_CONFIG
+            );
+        });
+        $container->bind('PUBG_API_KEY', function () {
+            require base_path("/config.php");
+            return $PUBG_API_KEY;
+        });
+        return $container;
+    }
+    public static function resolve($key)
+    {
+        $container = self::setContainer();
+        return $container->resolve($key);
+    }
+}
