88561ac50afb11d53c285325e5d5f1eb5fd18770
Jason Suarez <swarecito@gmail.com>
2024-07-16 22:26:06 +0200

    chore: Update registration form to display error messages for email and password fields

--- views/registration/register.view.php
+++ views/registration/register.view.php
@@ -12,22 +12,29 @@
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
+                <form class="space-y-6" method="POST">
+                    <input type="hidden" name="">
                     <div>
                         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div class="mt-2">
                             <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
+                        <?php if (isset($errors['email'])) : ?>
+                            <p class="text-red-500 text-sm"><?= $errors['email'] ?></p>
+                        <?php endif; ?>
                     </div>
 
                     <div>
                         <div class="flex items-center justify-between">
                             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                           
+
                         </div>
                         <div class="mt-2">
                             <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
+                        <?php if (isset($errors['password'])) : ?>
+                            <p class="text-red-500 text-sm"><?= $errors['password'] ?></p>
+                        <?php endif; ?>
                     </div>
 
                     <div>
@@ -35,7 +42,7 @@
                     </div>
                 </form>
 
-              
+
             </div>
         </div>
     </div>
