258d72e3a3c3b3765add3e1addba91085405095f
Jason Suarez <swarecito@gmail.com>
2024-07-16 09:08:22 +0200

    refactor: Update getPlayerSeasonStats function to handle cURL errors

--- functions.php
+++ functions.php
@@ -10,29 +10,33 @@ function dd($data)
 
 function getPlayerSeasonStats($playerOne, $seasonId, $PUBG_API_KEY)
 {
-    // Init cURL session
-    $url = "https://api.pubg.com/shards/xbox/seasons/{$seasonId}/gameMode/squad/players?filter[playerIds]={$playerOne->id}";
-    $ch = curl_init($url);
+    try {
+        // Init cURL session
+        $url = "https://api.pubg.com/shards/xbox/seasons/{$seasonId}/gameMode/squad/players?filter[playerIds]={$playerOne->id}";
+        $ch = curl_init($url);
 
-    // Set the HTTP headers and options
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Accept: application/vnd.api+json',
-        'Authorization: Bearer ' . $PUBG_API_KEY,
-    ]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        // Set the HTTP headers and options
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/vnd.api+json',
+            'Authorization: Bearer ' . $PUBG_API_KEY,
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
-    $response = curl_exec($ch);
-    if ($response === false) {
-        throw new Exception(curl_error($ch), curl_errno($ch));
+        $response = curl_exec($ch);
+        if ($response === false) {
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        }
+
+        $responseData = json_decode($response, true);
+        $playerSeasonStats = new PlayerSeason(json_encode($responseData));
+        $playerSeasonStats = $playerSeasonStats->data[0];
+        curl_close($ch);
+
+        return $playerSeasonStats;
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
-
-    $responseData = json_decode($response, true);
-    $playerSeasonStats = new PlayerSeason(json_encode($responseData));
-    $playerSeasonStats = $playerSeasonStats->data[0];
-    curl_close($ch);
-
-    return $playerSeasonStats;
 }
 
 function getPlayer(
