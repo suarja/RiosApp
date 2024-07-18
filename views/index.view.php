e5cda0f8250cd0997098814cf6c134caa4daaa90
Jason Suarez <swarecito@gmail.com>
2024-07-18 11:15:55 +0200

    refactor views: remove unnecessary code and fix missing closing tags
    The code changes in the `views` directory involve removing unnecessary code and fixing missing closing tags in the `index.view.php` and `head.php` files. This refactor improves the code structure and ensures proper rendering of the HTML elements.

--- views/index.view.php
+++ views/index.view.php
@@ -1,4 +1,2 @@
 <?php require base_path("/views/partials/head.php"); ?>
 
-
-<?php require base_path("/views/partials/footer.php"); ?>
--- views/partials/head.php
+++ views/partials/head.php
@@ -22,3 +22,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <title><?= htmlspecialchars($heading); ?></title>
 </head>
+
+</html>
