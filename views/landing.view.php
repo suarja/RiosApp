a1f1cf616f8c9608968eec9752a877f1c95afc44
Jason Suarez <swarecito@gmail.com>
2024-07-21 07:52:01 -0400

    Change subtitle in landing page

--- views/landing.view.php
+++ views/landing.view.php
@@ -36,7 +36,7 @@
 
           </h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">
-            The most trash team in PUBG
+            The most trash team in PUBG history
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="/player" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Player Stats</a>
